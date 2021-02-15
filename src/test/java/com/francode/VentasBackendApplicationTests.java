05f16be4134f317a4f032ec0edd14909dde06bdf
unknown <ftadeo.jeri@gmail.com>
2021-02-15 18:51:54 -0500

    Comienzo del Proyecto

---
+++ src/test/java/com/francode/VentasBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.francode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VentasBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
